3fe59f95795232c9de7f3dda8b06cd7edcd1824d
Emily Port <emilylport@gmail.com>
2023-01-04 14:45:10 -0700

    spec file for disease case facade

--- spec/facades/disease_case_facade_spec.rb
+++ spec/facades/disease_case_facade_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe DiseaseCaseFacade do 
-  
+RSpec.describe DiseaseCaseFacade, :vcr, type: :facade do 
+  describe 'creating disease cases' do 
+    it 'it creates an array of objects' do 
+      facade = DiseaseCaseFacade.states_diseases("GEORGIA")
+
+      expect(facade).to be_an Array 
+      expect(facade[0]).to be_an_instance_of(DiseaseCase)
+      expect(facade[0].state).to eq("GEORGIA")
+    end
+  end
 end
