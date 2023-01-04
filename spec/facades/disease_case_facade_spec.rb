25d71531d107b85794ee24a0c198e40261e33024
Emily Port <emilylport@gmail.com>
2023-01-04 14:47:21 -0700

    add tests to disease case facade spec

--- spec/facades/disease_case_facade_spec.rb
+++ spec/facades/disease_case_facade_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe DiseaseCaseFacade, :vcr, type: :facade do
       expect(facade).to be_an Array 
       expect(facade[0]).to be_an_instance_of(DiseaseCase)
       expect(facade[0].state).to eq("GEORGIA")
+      expect(facade[0].state).to_not eq("COLORADO")
+
+      expect(facade[0].current_week).to eq("1")
+      expect(facade[0].disease).to eq("Anthrax")
+      expect(facade[0].current_week_cases).to eq("0")
+
     end
   end
 end
