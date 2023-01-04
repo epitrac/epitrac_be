b74ae34a32b1f400bad9dab3821df4052a96e536
Emily Port <emilylport@gmail.com>
2023-01-04 14:53:33 -0700

    add spec for diseasecase poro

--- spec/poros/disease_case_spec.rb
+++ spec/poros/disease_case_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe DiseaseCase do 
-  
+RSpec.describe DiseaseCase, :vcr, type: :poros do
+  describe 'it returns disease case objects' do 
+    it 'response is returned in disease case objects with defined attributes' do 
+      diseases = DiseaseCaseFacade.states_diseases("GEORGIA")
+      expect(diseases).to be_an Array 
+
+      disease = diseases.first 
+
+      expect(disease).to be_an_instance_of DiseaseCase
+      expect(disease.current_week).to be_a String 
+      expect(disease.state).to be_a String 
+      expect(disease.year).to be_a String 
+      expect(disease.geocode).to be_a Hash  
+
+      
+    end
+  end
+
+
 end
