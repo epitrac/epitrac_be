807112afa10629f54a7dff40156a87e911b1b3de
Emily Port <emilylport@gmail.com>
2023-01-08 12:43:29 -0700

    update disease model spec to test for partial match and case insensitive disease info endpoint by disease

--- spec/models/disease_spec.rb
+++ spec/models/disease_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Disease, type: :model do
     it { should validate_presence_of :information }
     it { should validate_presence_of :link }
   end
+
+  describe '#partial_match' do 
+    it 'disease can be a partial match and case insensitive' do 
+      disease_info = create(:disease, disease: "Anthrax")
+      other_disease_info = create(:disease, disease: "Syphilis")
+
+      expect(Disease.partial_match("Anthr")[0]).to eq(disease_info)
+      expect(Disease.partial_match("Anthr")[0]).to_not eq(other_disease_info)
+
+
+      expect(Disease.partial_match("SYPHIL")[0]).to eq(other_disease_info)
+      expect(Disease.partial_match("SYPHIL")[0]).to_not eq(disease_info)
+
+    end
+  end
+
+
 end
