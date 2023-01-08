e5afa36bce90c57771babec83c7b3dcb2725e285
Emily Port <emilylport@gmail.com>
2023-01-08 14:24:46 -0700

    update user articles controller

--- app/controllers/api/v1/user_articles_controller.rb
+++ app/controllers/api/v1/user_articles_controller.rb
@@ -6,10 +6,12 @@ class Api::V1::UserArticlesController < ApplicationController
       render json: {error: "cannot find saved articles without a user id"}, status: 404
 
     end
-    end
+  end
+
   def create
     render json: UserArticleSerializer.new(UserArticle.create!(user_article_params)), status: 201
   end
+  
   def destroy
     if UserArticle.exists?(params[:id])
       render json: UserArticle.delete(params[:id])
@@ -20,7 +22,6 @@ class Api::V1::UserArticlesController < ApplicationController
     #   user_article = UserArticle.where(user_id: params[:user_id], article_id: params[:article_id])
     #   render json: UserArticle.delete(user_article)
     # else 
-
   end
 
 private
