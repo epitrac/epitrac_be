d08b68787a10a4b715a8ba8ebd2381a7bd8c623a
Emily Port <emilylport@gmail.com>
2023-01-09 08:44:19 -0700

    update disease info to include short name

--- app/controllers/api/v1/disease_info_controller.rb
+++ app/controllers/api/v1/disease_info_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DiseaseInfoController < ApplicationController
   def index
-    if params[:disease]
-      diseases = Disease.partial_match(params[:disease])
+    if params[:short_name]
+      diseases = Disease.partial_match(params[:short_name])
       render json: DiseaseInfoSerializer.new(diseases)
     else
       render json: {error: "cannot return disease info without a disease"}, status: 404
--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,7 +1,7 @@
 class Disease < ApplicationRecord
-  validates_presence_of :disease, :information, :link
+  validates_presence_of :disease, :information, :link, :short_name
 
-  def self.partial_match(disease)
-    Disease.where("disease ILIKE?", "%#{disease}%")
+  def self.partial_match(short_name)
+    Disease.where("short_name ILIKE?", "%#{short_name}%")
   end
 end
