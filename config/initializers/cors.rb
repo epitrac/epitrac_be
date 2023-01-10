4882e950f6164e9d571c7bd489fa36199b49e61c
Darby Smith <newman.darby2@gmail.com>
2023-01-10 16:37:44 -0500

    Configure cors for frontend app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://epitrac.herokuapp.com/'
 
     resource '*',
       headers: :any,
