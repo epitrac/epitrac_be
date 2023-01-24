730c829c4a9cd189f1f14b1336f56d35a211c88c
Emily Port <emilylport@gmail.com>
2023-01-24 14:05:15 -0700

    add logic for sad path if disease has no articles

--- app/facades/article_facade.rb
+++ app/facades/article_facade.rb
@@ -1,8 +1,18 @@
 class ArticleFacade 
   def self.articles(disease)
-    ArticleService.articles_by_disease(disease).map do |article_data|
-      Article.new(article_data)
+    article_data = ArticleService.articles_by_disease(disease)
+    # require 'pry'; binding.pry
+    if article_data.count <= 4 || article_data == []   
+      []
+    else 
+      article_data.map do |article_data|
+        Article.new(article_data)
+      end
     end
+    
+    # article_data.map do |article_data|
+    #   Article.new(article_data)
+    # end
   end
 
 end
