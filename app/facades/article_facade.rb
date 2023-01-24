e1f0597a2e8b4dc59dd13a4062d0d06b0c999c30
Emily Port <emilylport@gmail.com>
2023-01-24 14:26:26 -0700

    update vcr

--- app/facades/article_facade.rb
+++ app/facades/article_facade.rb
@@ -1,18 +1,12 @@
 class ArticleFacade 
   def self.articles(disease)
     article_data = ArticleService.articles_by_disease(disease)
-    # require 'pry'; binding.pry
-    if article_data.count <= 4 || article_data == []   
+    if article_data[0] == nil || article_data == []   
       []
     else 
       article_data.map do |article_data|
         Article.new(article_data)
       end
     end
-    
-    # article_data.map do |article_data|
-    #   Article.new(article_data)
-    # end
   end
-
 end
